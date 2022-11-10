31206d3ca026921907135641cd4a772f63985eb9
NicolasDouillet <nicolas.douillet@free.fr>
2022-11-10 09:15:53 +0100

    update sphere + rename files before move

--- doc.m
+++ doc.m
@@ -1,25 +1,26 @@
-%% tetrahedron_circumscribed_sphere
+%% tetrahedron_circumsphere
 %
 % Function to compute the centre and the radius of
 % the sphere circumscribed to a given tetrahedron.
 %
-% Author & support : nicolas.douillet (at) free.fr, 2017-2021.
+% Author & support : nicolas.douillet (at) free.fr, 2017-2022.
 %
 %% Syntax
-% C = tetrahedron_circumscribed_sphere(V);
+% C = tetrahedron_circumsphere(V);
 %
-% [C, r] = tetrahedron_circumscribed_sphere(V);
+% [C, r] = tetrahedron_circumsphere(V);
 %
 %% Description
-% C = tetrahedron_circumscribed_sphere(V) computes coordinates of C, which is the
+% C = tetrahedron_circumsphere(V) computes coordinates of C, which is the
 % centre of the circumscribed sphere to the tetrahedron V.
 %
-% [C, r] = tetrahedron_circumscribed_sphere(V) also returns the radius of the
+% [C, r] = tetrahedron_circumsphere(V) also returns the radius of the
 % circumscribed sphere.
 %
 %% See also
 %
-% <https://fr.mathworks.com/matlabcentral/fileexchange/73490-point-to-plane-distance?s_tid=prof_contriblnk point_to_plane_distance>
+% | <https://fr.mathworks.com/matlabcentral/fileexchange/119788-triangle-circumcircle-3d-2d?s_tid=srchtitle triangle_circumcircle> |
+% <https://fr.mathworks.com/matlabcentral/fileexchange/73490-point-to-plane-distance?s_tid=prof_contriblnk point_to_plane_distance> |
 %
 %% Input argument
 %
@@ -39,23 +40,20 @@
 %
 % Random tetrahedron
 V = 2*(rand(3,4)-0.5);
-[C, radius] = tetrahedron_circumscribed_sphere(V);
-theta = (0:pi/10:pi)';
-phi = (0:pi/10:2*pi);
-xS = radius*sin(theta)*cos(phi);
-yS = radius*sin(theta)*sin(phi);
-zS = repmat(radius*cos(theta),1,length(phi));
+[C, radius] = tetrahedron_circumsphere(V);
+[Sx,Sy,Sz] = sphere(60);
 figure;
 set(gcf,'Color',[0 0 0]);
-plot3(V(1,:),V(2,:),V(3,:),'g.','Linewidth',5), hold on;
 plot3(C(1,1),C(2,1),C(3,1),'ro','Linewidth',5), hold on;
 couples = combnk(1:4,2);
 for k = 1:size(couples,1)
-    line(V(1,couples(k,:)),V(2,couples(k,:)),V(3,couples(k,:)), 'Color', [0 1 0], 'Linewidth',2), hold on;
+    line(V(1,couples(k,:)),V(2,couples(k,:)),V(3,couples(k,:)), 'Color', [0 0 1], 'Linewidth',2), hold on;
 end
-plot3(C(1,1)+xS(:,1:end),C(2,1)+yS(:,1:end),C(3,1)+zS(:,1:end),'w-.'), hold on;
-plot3(C(1,1)+yS(1:end,:)',C(2,1)+xS(1:end,:)',C(3,1)+zS(1:end,:)','w-.'), hold on;
-set(gca,'Color',[0 0 0]);
+surf(radius*Sx+C(1,1),radius*Sy+C(2,1),radius*Sz+C(3,1)), shading interp, 
+set(gca,'Color',[0 0 0],'XColor',[1 1 1],'YColor',[1 1 1],'ZColor',[1 1 1],'FontSize',16);
+colormap([0 1 1]);
+alpha(0.3);
+camlight left;
 axis equal, axis tight;
 
 %% Example #2
@@ -66,4 +64,4 @@ V2 = [-sqrt(2)/3 sqrt(6)/3 -1/3];
 V3 = [-sqrt(2)/3 -sqrt(6)/3 -1/3];
 V4 = [0 0 -1/3];
 V = [V1' V2' V3' V4'];
-[C, radius] = tetrahedron_circumscribed_sphere(V);
+[C, radius] = tetrahedron_circumsphere(V);
